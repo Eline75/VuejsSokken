9cfe98d1de8d777101803619109091cab61e39d6
Eline <e.eijkelenkamp@gmail.com>
2019-02-07 14:19:13 +0100

    event Handling

--- main.js
+++ main.js
@@ -1,10 +1,33 @@
 var app = new Vue({
   el: "#app",
-
   data: {
     product: "Socks",
-    image: "vmSocks-green.jpg",
-    inventory: 0
+    image: ":vmSocks-green.jpg",
+    inStock: true,
+    details: ["80% cotton", "20% polyester", "Gender-neutral"],
+    variants: [
+      {
+        variantId: 2234,
+        variantColor: "green",
+        variantImage: "vmSocks-green.jpg"
+      },
+      {
+        variantId: 2235,
+        variantColor: "blue",
+        variantImage: "vmSocks-blue.jpg"
+      }
+    ],
+    cart: 0
+  },
+  methods: {
+    addToCart() {
+      this.cart += 1;
+    },
+    updateProduct(variantImage) {
+      this.image = variantImage;
+    },
+    removeFromCart() {
+      this.cart -= 1;
+    }
   }
 });
-var product = "Socks";
